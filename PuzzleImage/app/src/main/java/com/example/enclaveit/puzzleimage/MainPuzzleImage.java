9f0e75e84cee58cbb2ac1aef05c0c41ae5389ca8
danisluis6 <vuongluis@hotmail.com>
2016-12-12 11:20:20 +0700

    ok

---
+++ PuzzleImage/app/src/main/java/com/example/enclaveit/puzzleimage/MainPuzzleImage.java
@@ -0,0 +1,13 @@
+package com.example.enclaveit.puzzleimage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainPuzzleImage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_puzzle_image);
+    }
+}
