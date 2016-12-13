4243f4cd40c3d1704d6ed84e2bb81eadf16ad1b0
danisluis6 <vuongluis@hotmail.com>
2016-12-13 09:14:43 +0700

    ok

---
+++ PuzzleActivity/app/src/main/java/com/example/enclaveit/puzzleactivity/PuzzleActivity.java
@@ -0,0 +1,13 @@
+package com.example.enclaveit.puzzleactivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PuzzleActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_puzzle);
+    }
+}
